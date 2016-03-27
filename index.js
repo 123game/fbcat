ca21d506bdac0d5d8f911ddcf1ec686534a92476
123gamestudio <123game.123game.123game@gmail.com>
2016-03-27 21:25:04 +0700

    Atom Beauty

--- index.js
+++ index.js
@@ -7,32 +7,32 @@ var https = require('https');
  * @return callback {bool}
  */
 module.exports = {
-	check: function(user_id, user_access_token, app_access_token, callback) {
+  check: function(user_id, user_access_token, app_access_token, callback) {
 
-		if (!user_id || !user_access_token || !app_access_token) {
-				callback(false);
-		}
+    if (!user_id || !user_access_token || !app_access_token) {
+      callback(false);
+    }
 
-		var options = {
-			host: 'graph.facebook.com',
-			path: '/debug_token?input_token=' +	user_access_token + '&access_token=' + app_access_token
-		};
+    var options = {
+      host: 'graph.facebook.com',
+      path: '/debug_token?input_token=' + user_access_token + '&access_token=' + app_access_token
+    };
 
-		https.get(options, function(res) {
-			var string_data = '';
+    https.get(options, function(res) {
+      var string_data = '';
 
-			res.on('data', function(chunk) {
-				string_data += chunk;
-			});
+      res.on('data', function(chunk) {
+        string_data += chunk;
+      });
 
-			res.on('end', function() {
-				var json_data = JSON.parse(string_data);
-				if (json_data.data && json_data.data.is_valid && json_data.data.user_id == user_id) {
-					callback(true);
-				} else {
-					callback(false);
-				}
-			});
-		});
-	}
+      res.on('end', function() {
+        var json_data = JSON.parse(string_data);
+        if (json_data.data && json_data.data.is_valid && json_data.data.user_id == user_id) {
+          callback(true);
+        } else {
+          callback(false);
+        }
+      });
+    });
+  }
 };
